9fc568749f0c61f0d4587a5c96182d15613197e9
w2350946792 <w2350946792@163.com>
2017-07-27 08:04:55 +0800

    add

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports={
+	entry:'./js/entry.js',
+	output:{
+		filename:'./bundle.js',
+	},
+	module:{
+		loaders:[
+			{
+				test:/\.css$/,
+				loader:'style-loader!css-loader'
+			}
+		]
+	}
+}
